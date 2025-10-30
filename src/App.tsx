54117e863db7a01ced42cfc6d28388539c6cad24
Gayuh Jati <gayuhjati1234@gmail.com>
2025-10-30 14:44:58 +0700

    reinstall modul

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -18,7 +19,6 @@ import ProfileModal from "@/components/ProfileModal";
 import Validation from "./pages/Validation";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import React from "react";
 
 const queryClient = new QueryClient();
 
